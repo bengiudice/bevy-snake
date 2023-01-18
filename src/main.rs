62d39a857bfd9a1da842e40f89a0cb9b6e6db0f6
benjamin giudice <ben.giudice@gmail.com>
2023-01-18 14:56:40 +0800

    Refactor to custom grid scaling

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,12 @@ fn main() {
         .add_startup_system(setup_camera)
         .add_startup_system(spawn_snake)
         .add_system(snake_movement)
+        .add_system_set_to_stage(
+            CoreStage::PostUpdate,
+            SystemSet::new()
+                .with_system(position_translation)
+                .with_system(size_scaling),
+        )
         .add_plugins(DefaultPlugins)
         .run();
 }
@@ -13,6 +19,26 @@ fn setup_camera(mut commands: Commands) {
 #[derive(Component)]
 struct SnakeHead;
 const SNAKE_HEAD_COLOR: Color = Color::rgb(0.7, 0.7, 0.7);
+const ARENA_WIDTH: u32 = 10;
+const ARENA_HEIGHT: u32 = 10;
+#[derive(Component, Clone, Copy, PartialEq, Eq)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+#[derive(Component)]
+struct Size {
+    width: f32,
+    height: f32,
+}
+impl Size {
+    pub fn square(x: f32) -> Self {
+        Self {
+            width: x,
+            height: x,
+        }
+    }
+}
 fn spawn_snake(mut commands: Commands) {
     commands
         .spawn(SpriteBundle {
@@ -26,21 +52,47 @@ fn spawn_snake(mut commands: Commands) {
             },
             ..default()
         })
-        .insert(SnakeHead);
+        .insert(SnakeHead)
+        .insert(Position { x: 3, y: 3 })
+        .insert(Size::square(0.8));
 }
-fn snake_movement(key: Res<Input<KeyCode>>, mut heads: Query<&mut Transform, With<SnakeHead>>) {
-    for mut tx in heads.iter_mut() {
+fn size_scaling(windows: Res<Windows>, mut q: Query<(&Size, &mut Transform)>) {
+    let window = windows.get_primary().unwrap();
+    for (sprite, mut tx) in q.iter_mut() {
+        tx.scale = Vec3::new(
+            sprite.width / ARENA_WIDTH as f32 * window.width() as f32,
+            sprite.height / ARENA_HEIGHT as f32 * window.height(),
+            1.0,
+        );
+    }
+}
+fn position_translation(windows: Res<Windows>, mut q: Query<(&Position, &mut Transform)>) {
+    fn convert(pos: f32, bound_window: f32, bound_game: f32) -> f32 {
+        let tile_size = bound_window / bound_game;
+        pos / bound_game * bound_window - bound_window / 2.0 + tile_size / 2.0
+    }
+    let window = windows.get_primary().unwrap();
+    for (pos, mut transform) in q.iter_mut() {
+        transform.translation = Vec3::new(
+            convert(pos.x as f32, window.width() as f32, ARENA_WIDTH as f32),
+            convert(pos.y as f32, window.height() as f32, ARENA_HEIGHT as f32),
+            0.0,
+        );
+    }
+}
+fn snake_movement(key: Res<Input<KeyCode>>, mut head_positions: Query<&mut Position, With<SnakeHead>>) {
+    for mut pos in head_positions.iter_mut() {
         if key.pressed(KeyCode::Left) {
-            tx.translation.x -= 2.0;
+            pos.x -= 1;
         }
         if key.pressed(KeyCode::Right) {
-            tx.translation.x += 2.0;
+            pos.x += 1;
         }
         if key.pressed(KeyCode::Down) {
-            tx.translation.y -= 2.0;
+            pos.y -= 1;
         }
         if key.pressed(KeyCode::Up) {
-            tx.translation.y += 2.0;
+            pos.y += 1;
         }
     }
 }
